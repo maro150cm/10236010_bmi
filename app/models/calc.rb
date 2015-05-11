0e7a78c07b4b153ea4be55a12a6a621c22c51a38
maro150cm <haveagoodday8496@gmail.com>
2015-05-11 03:48:10 +0000

    model change

--- app/models/calc.rb
+++ app/models/calc.rb
@@ -3,5 +3,5 @@ class Calc < ActiveRecord::Base
     validates :body_height, presence: { message: '請填寫身高' },
               inclusion: 50..250
     validates :body_weight, presence: { message: '請填寫體重' },
-              inclusion: 50..250
+              inclusion: 20..250
 end
